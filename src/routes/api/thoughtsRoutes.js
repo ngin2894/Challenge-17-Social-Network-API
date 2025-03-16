76b0bd94596b55f2d7f3a3d7b75ab5e54974c468
ngin2894 <nataliegindraux@gmail.com>
2025-03-16 13:07:10 -0500

    Added all of the thoughtsRoutes

---
+++ src/routes/api/thoughtsRoutes.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { getAllThoughts, getThoughtById, createThought, updateThought, deleteThought, addReaction, deleteReaction } from '../../controllers/thoughtsController.js';
+
+const thoughtsRouter = Router();
+
+thoughtsRouter.route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+thoughtsRouter.route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+thoughtsRouter.route('/:thoughtId/reactions')
+    .post(addReaction);
+
+thoughtsRouter.route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction);
+
+export default thoughtsRouter;
