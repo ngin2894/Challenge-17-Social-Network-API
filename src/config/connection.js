b0840ea01252aaa5a5088877ffba75149305da05
ngin2894 <nataliegindraux@gmail.com>
2025-03-16 12:20:58 -0500

    added code to the connection.js file

---
+++ src/config/connection.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const db = async () =>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api');
+        console.log('Database connected.');
+        return mongoose.connection;
+    } catch(error) {
+        console.error('Database connection error:', error);
+        throw new Error('Database connection failed.');
+    }
+}
+
+export default db;
